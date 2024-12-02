9fcdbef1abe3e7ab626b3537ac259edc73ff95fc
Nucl3arsn3k <shredderofwheat@gmail.com>
2024-12-02 11:16:19 -0500

    New staged commit with working conversion

--- lib/num_utilities.ex
+++ lib/num_utilities.ex
@@ -1,4 +1,5 @@
 defmodule NumUtilities do
+
   def binhelp(x) do
     list = []
     binarymanual(x,list)
@@ -11,10 +12,13 @@ defmodule NumUtilities do
     if x >= 2 do #was going to use match,but this is more legible
       c = div(x, 2)  #need to do this,not float division
       b = rem(x, 2)
-      binarymanual(c,[b|ints])#prepend,will reverse list eventually. Doing this because append is expensive.
+      binarymanual(c,[b|ints])#prepend,reverse the reverse
     else
-      reverse = Enum.reverse(ints)
-      x
+      add = [x | ints]
+      #reverse = Enum.reverse(add)
+
+      add
+
     end
   end
 end
--- lib/num_waterfall.ex
+++ lib/num_waterfall.ex
@@ -2,7 +2,9 @@
 defmodule NumWaterfall do
 
   def main do
-    random_number = :rand.uniform(5000)
-    NumUtilities.binhelp(random_number)
+    #random_number = :rand.uniform(5000)
+    val = NumUtilities.binhelp(6)
+    IO.inspect(val, label: "val")
+    IO.puts("num is #{inspect(val)}")
   end
 end
