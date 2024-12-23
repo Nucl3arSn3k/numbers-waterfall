033fc5b2028bd69f6ca3de7d27977231f516545a
Nucl3arSn3k <shredderofwheat@gmail.com>
2024-12-22 22:57:18 -0500

    tweaked rescursion

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,11 @@
 
 -- Just divides,no side effects 
 binaryfunct :: Int -> [Int]
-binaryfunct 0 = [0]           -- base case: when input is 0
+binaryfunct 0 = []           -- base case: when input is 0
 binaryfunct x = binaryfunct (x `div` 2) ++ [x `mod` 2]
 
--- IO function - interacts with the world
+-- IO function - interacts with the world,so side effects
 main :: IO ()
 main = do
-  let result = binaryfunct 10
+  let result = binaryfunct 5
   print result
