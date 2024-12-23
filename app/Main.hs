f08312600901aeca45c1194152bc24910cd56bce
Nucl3arSn3k <shredderofwheat@gmail.com>
2024-12-23 15:10:49 -0500

    Added -threaded flag for UI functionality

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,12 @@
 module Main where
-
+import Brick
 
 convert :: Int -> [Int]
 convert 0 = []
 convert x = convert (x `div` 2) ++ [x `mod` 2]
 
-
+ui :: Widget()
+ui = str "Hey"
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = simpleMain ui
